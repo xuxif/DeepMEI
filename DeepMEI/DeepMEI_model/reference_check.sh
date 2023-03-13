ec18b8b20993b64fe43404557d32c14e8409ee60
xuxf <2623716586@qq.com>
2023-03-13 17:42:10 +0800

    Debug

--- DeepMEI/DeepMEI_model/reference_check.sh
+++ DeepMEI/DeepMEI_model/reference_check.sh
@@ -1,16 +1,16 @@
 bam_file=$1
 REF=$2
-chr=`samtools view -H $bam_file $REF |grep '^@SQ' |grep -E "SN:chr1|SN:1"$'\t'|cut -f2|perl -npe "s/SN://"|perl -F'\t' -alne 'if($_=~/chr/) {print "chr";} else {print "nonchr";}'`
-version=`samtools view -H $bam_file $REF |grep '^@SQ' |grep -E "SN:chr1|SN:1"$'\t'|cut -f3|perl -npe "s/LN://"|perl -F'\t' -alne 'if($F[0]==248956422) {print "38";} elsif($F[0]==249250621) {print "19"} else {print "0";}'`
-len_bam=`samtools view -H $bam_file $REF |grep '^@SQ' |grep -E "SN:chr1|SN:1"$'\t'|cut -f3|perl -npe "s/LN://"`
+chr=`samtools view -H $bam_file $REF |grep '^@SQ' |grep -E "SN:chr1"$'\t'"|SN:1"$'\t'|cut -f2|perl -npe "s/SN://"|perl -F'\t' -alne 'if($_=~/chr/) {print "chr";} else {print "nonchr";}'`
+version=`samtools view -H $bam_file $REF |grep '^@SQ' |grep -E "SN:chr1"$'\t'"|SN:1"$'\t'|cut -f3|perl -npe "s/LN://"|perl -F'\t' -alne 'if($F[0]==248956422) {print "38";} elsif($F[0]==249250621) {print "19"} else {print "0";}'`
+len_bam=`samtools view -H $bam_file $REF |grep '^@SQ' |grep -E "SN:chr1"$'\t'"|SN:1"$'\t'|cut -f3|perl -npe "s/LN://"`
 dict_file=`echo $REF|perl -npe "s/\.fa//;s/\.fasta//"`
 if [[ ! -f "${dict_file}.dict" ]] && [[ ! -f "${REF}.dict" ]]
 	then
 		echo "The dictionary file for the reference genome(${REF}.dict or ${dict_file}.dict) was not found."
 	exit
 fi
-len_dict=`cat ${REF}.dict ${dict_file}.dict 2>/dev/null|grep '^@SQ' |grep -E "SN:chr1|SN:1"$'\t'|cut -f3|perl -npe "s/LN://"`
-dict_chr=`cat ${REF}.dict ${dict_file}.dict 2>/dev/null|grep '^@SQ' |grep -E "SN:chr1|SN:1"$'\t'|cut -f2|perl -npe "s/SN://"|perl -F'\t' -alne 'if($_=~/chr/) {print "chr";} else {print "nonchr";}'`
+len_dict=`cat ${REF}.dict ${dict_file}.dict 2>/dev/null|grep '^@SQ' |grep -E "SN:chr1"$'\t'"|SN:1"$'\t'|cut -f3|perl -npe "s/LN://"`
+dict_chr=`cat ${REF}.dict ${dict_file}.dict 2>/dev/null|grep '^@SQ' |grep -E "SN:chr1"$'\t'"|SN:1"$'\t'|cut -f2|perl -npe "s/SN://"|perl -F'\t' -alne 'if($_=~/chr/) {print "chr";} else {print "nonchr";}'`
 if [[ $version -eq 0 ]]
 then
 	echo "Reference sequence length is not match GRCh38, GRCh37 or hg19."
